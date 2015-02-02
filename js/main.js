b5d95d5a2fcfde08efb290413f85c8b0a05a5b11
Greg Back <gback@mitre.org>
2015-02-02 10:25:01 -0600

    Add links to section headers.

--- js/main.js
+++ js/main.js
@@ -43,3 +43,15 @@ $(document).ready(function() {
     $('.idiom-construct').tooltip()
     $('#globe').tooltip()
 });
+
+$(function() {
+  return $("h2, h3, h4, h5, h6").each(function(i, el) {
+    var $el, icon, id;
+    $el = $(el);
+    id = $el.attr('id');
+    icon = '<i class="glyphicon glyphicon-link"></i>';
+    if (id) {
+      return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+    }
+  });
+});
